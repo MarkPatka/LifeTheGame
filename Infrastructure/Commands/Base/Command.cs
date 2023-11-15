b34c338548fa5875615d15797842a8cc1792b112
usr <usr@PC1>
2023-11-15 17:26:29 +0300

    Creates Command Abstraction and Implement it

---
+++ Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Life_The_Game.Infrastructure.Commands.Base;
+
+internal abstract class Command : ICommand
+{
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    private bool executable = true;
+    public bool Executable
+    {
+        get => executable;
+        set
+        {
+            if (executable == value) return;
+
+            executable = value;
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+
+    bool ICommand.CanExecute(object? parameter) => executable && CanExecute(parameter);
+    void ICommand.Execute(object? parameter)
+    {
+        if (CanExecute(parameter))
+            Execute(parameter);
+    }
+    public virtual bool CanExecute(object? parameter) => true;
+    public abstract void Execute(object? parameter);
+}
---
+++ Infrastructure/Commands/DelegateCommand.cs
@@ -0,0 +1,27 @@
+﻿using Life_The_Game.Infrastructure.Commands.Base;
+using System;
+
+namespace Life_The_Game.Infrastructure.Commands;
+
+internal class DelegateCommand : Command
+{
+    private readonly Action<object?> execute;
+    private readonly Func<object?, bool>? canExecute;
+
+    public DelegateCommand(Action Execute, Func<bool>? CanExecute = null)
+        : this(Execute: p => Execute(),
+              CanExecute: CanExecute is null ? null : p => CanExecute())
+    {
+
+    }
+
+    public DelegateCommand(Action<object?> Execute, Func<object?, bool>? CanExecute)
+    {
+        execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+        canExecute = CanExecute;
+    }
+
+    public override bool CanExecute(object? parameter) => canExecute?.Invoke(parameter) ?? true;
+    public override void Execute(object? parameter) => execute(parameter);
+
+}
