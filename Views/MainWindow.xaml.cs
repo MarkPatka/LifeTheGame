a2b168f05e19d686ed0ed15c3055c231b4f32a45
usr <usr@PC1>
2023-11-15 17:26:54 +0300

    Connect MainVm to GameFieldVm

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,46 +1,44 @@
-﻿using Life_The_Game.Views.Controls;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
 
 
-namespace Life_The_Game.Views
+namespace Life_The_Game.Views;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2)
         {
-            InitializeComponent();
+            if (WindowState == WindowState.Maximized
+                || SystemParameters.WorkArea.Height - 1 - this.ActualHeight < 1 
+                || SystemParameters.WorkArea.Width - this.ActualWidth < 1)
+            {
+                WindowState = WindowState.Normal;
+                Height = 800;
+                Width = 1000;
+            }
+            else
+            {
+                Left = 0;
+                Top = 0;
+                Height = SystemParameters.WorkArea.Height - 1;
+                Width = SystemParameters.WorkArea.Width;
+            }
         }
 
-        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        if (e.ChangedButton == MouseButton.Left)
         {
-            if (e.ClickCount == 2)
+            try
             {
-                if (WindowState == WindowState.Maximized
-                    || SystemParameters.WorkArea.Height - 1 - this.ActualHeight < 1 
-                    || SystemParameters.WorkArea.Width - this.ActualWidth < 1)
-                {
-                    WindowState = WindowState.Normal;
-                    Height = 800;
-                    Width = 1000;
-                }
-                else
-                {
-                    Left = 0;
-                    Top = 0;
-                    Height = SystemParameters.WorkArea.Height - 1;
-                    Width = SystemParameters.WorkArea.Width;
-                }
+                this.DragMove();
             }
-
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                try
-                {
-                    this.DragMove();
-                }
-                catch { }
-            }
+            catch { }
         }
     }
 }
